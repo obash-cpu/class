34e22af7cfee745301174c6c5f471e3e097385fa
obash-cpu <absalomobala@gmail.com>
2024-02-04 15:08:37 +0300

    Initial commit

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,19 @@
 import styles from './footer.module.css';
 
-function footer() {
-    return (
-        <div className={styles.footer}>
-            
-        </div>
-    )
-}
-export default footer;
+function Footer() {
+    const currentYear = new Date().getFullYear();
+    const copyrightYear = 2024;
+        function Footer() {
+            const currentYear = new Date().getFullYear();
+            const copyrightYear = 2024;
+          
+            return (
+              <div className={styles.footer}>
+                <footer>
+                <p>&copy; {copyrightYear === currentYear ? currentYear : `${copyrightYear}-${currentYear}`} Absalom Obala</p>
+              </footer>
+              </div>
+            );
+          }
+  }
+export default Footer;
